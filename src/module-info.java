eb7b8df1a0417b76ff153e668d6444fe9470292f
alarencinas <alarencinas@mbp-de-alar.home>
2022-11-12 14:51:26 +0100

    1

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author alarencinas
+ *
+ */
+module E2_Client {
+}
--- src/test/TEST.java
+++ src/test/TEST.java
@@ -1,7 +1,7 @@
 package test;
 
-public class Test {
+public class TEST {
 public static void main(String[] args) {
-	System.out.println("TEST");
+	System.out.println("test");
 }
 }
