9a54192ca7db239db7e7e193983550e2eb49a62d
ibarra18 <javier.ibarra@opendeusto.es>
2022-11-13 12:51:25 +0100

    LoginAppService apache

--- src/server/services/LoginAppService.java
+++ src/server/services/LoginAppService.java
@@ -1,5 +1,19 @@
 package server.services;
-
+import  server.data.domain.User;
 public class LoginAppService {
-
+	public User login(String email, String password) {
+		//TODO: Get User using DAO and check 		
+		User user = new User();		
+		user.setEmail("Javier.e2002@gmail.com");
+		user.setNickname("Javier");		
+		//Generate the hash of the password
+		String sha1 = org.apache.commons.codec.digest.DigestUtils.sha1Hex("$!9PhNz,");		
+		user.setPassword(sha1);
+		
+		if (user.getEmail().equals(email) && user.checkPassword(password)) {		
+			return user;
+		} else {
+			return null;
+		}
+	}
 }
