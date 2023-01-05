387a22b5d119f6340f63ebdcaa32ebef27c5e6ac
Aritz Candina <Aritz Candina@PCdeAritz>
2023-01-05 12:50:18 +0100

    commit

--- src/factory/LoginFactory.java
+++ src/factory/LoginFactory.java
@@ -7,7 +7,7 @@ public class LoginFactory {
 		if (userType.equals(LoginUserTypeDTO.Google)) {
 			return (ILoginGateway) new GoogleServiceGateway();
 		} else if (userType.equals(LoginUserTypeDTO.Facebook)) {
-			final String serverIP = "127.0.0.1";	//TODO not to hardcode it, beter with ant
+			final String serverIP = "127.0.0.1";	//TODO not to hardcode it, better with ant
 			final int serverPort = 35600;
 			return new FacebookSocketClient(serverIP, serverPort);
 		} else {
